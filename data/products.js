f483bde2a4c741009af86dc01805801585fc9115
VERNA990 <verenafonyuy11@gmail.com>
2026-01-30 01:17:28 +0100

    code cleaning

---
+++ data/products.js
@@ -0,0 +1,51 @@
+export const products = [{
+    image: 'assets/products/crochet-scarf.png',
+    name: 'Crochet Scarf',
+    description: 'soft,warm, and stylish. Perfect for the cold season',
+    rating: {
+        stars: 4.5,
+        count: 50
+    },
+    price: 'FCFA 8000'
+},
+{
+    image: 'assets/products/crochet-crop-top.png',
+    name: 'Crochet Crop Top',
+    description: 'Trendy and comfortable for summer vibes',
+    rating: {
+        stars: 5,
+        count: 500
+    },
+    price: 'FCFA 4000'
+},
+{
+    image: 'assets/products/crochet-earing.png',
+    name: 'Crochet Earing',
+    description: 'Look classy and unique',
+    rating: {
+        stars: 4,
+        count: 200
+    },
+    price: 'FCFA 2000'
+},
+{
+    image: 'assets/products/baby-set2.png',
+    name: 'Baby set',
+    description: 'Keep your baby feeling warm, cozy, protected and happy',
+    rating: {
+        stars: 5,
+        count: 100
+    },
+    price: 'FCFA 20000'
+},
+{
+    image: 'assets/products/crochet-beach-wear.png',
+    name: 'Beach Wear',
+    description: 'Who\'s ready to hit the beach. I tailor your beach wears for your comfort, boosting your confidence to the max.',
+    rating: {
+        stars: 4,
+        count: 60
+    },
+    price: 'FCFA 10000'
+}
+]
---
+++ data/services.js
@@ -0,0 +1,16 @@
+export const services = [{
+    image: 'assets/hero_images/mohamed-b-O1W2ZYGxFPA-unsplash.jpg',
+    name: 'Hair Braiding',
+    description: 'Expert braiding services including box braids, cornrows, natural hair twist and more. Each style crafted with precition and care'
+},
+{
+    image: 'assets/hero_images/istockphoto-526947125-612x612-1.jpg',
+    name: 'Custom Crochet',
+    description: 'Handmade crochet items made to order.From clothing to accesories, I create pieces that reflect your unique style.'
+},
+{
+    image: 'assets/hero_images/maria-kovalets-JEaKy0cP4OM-unsplash.jpg',
+    name: 'Ready-Made Shop',
+    description: 'Browse my collection of ready-to-deliver crochet items. Beautiful, handcrafted pieces available for immediate purchase'
+}
+]
